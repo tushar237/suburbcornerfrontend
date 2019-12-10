df17888861b1b0217ab9d637c860cbd7597581ae
tushar237 <tushargupta237@gmail.com>
2019-12-10 23:49:54 +0530

    Added button click events.

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -7,9 +7,14 @@ import { HttpClient } from '@angular/common/http';
 export class RestService {
 
   private usersApiUrl = "https://reqres.in/api/users?page=2";
+  private userApiUrl = "https://reqres.in/api/users/2";
   constructor(private http: HttpClient) { }
 
   getUsers() {
     return this.http.get(this.usersApiUrl);
+  }
+
+  getUser() {
+    return this.http.get(this.userApiUrl);
   }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -9,13 +9,21 @@ import { RestService } from '../rest.service';
 export class UsersComponent implements OnInit {
 
   public users = [];
+  public user;
   constructor(private restService : RestService) {}
 
   ngOnInit() {
+  }
+
+  getUsers(event) {
     this.restService.getUsers().subscribe((data) => {
       this.users = Array.from(Object.keys(data), k=>data[k]);
       console.log(this.users);
     });
+  }
+
+  getUser(event) {
+    this.user = this.restService.getUser();
   }
 
 }
